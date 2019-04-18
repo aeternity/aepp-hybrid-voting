704cd3c6efb68194b83d66e0b8dc88912d5f6ae6
Philipp Piwo <github@piwo.space>
2019-04-18 14:53:55 +0200

    added (breaking) tests

--- multisig-contract/test/multiSigTest.js
+++ multisig-contract/test/multiSigTest.js
@@ -15,44 +15,64 @@
  *  PERFORMANCE OF THIS SOFTWARE.
  */
 const Ae = require('@aeternity/aepp-sdk').Universal;
+const Crypto = require('@aeternity/aepp-sdk').Crypto;
 
 const config = {
-  host: "http://localhost:3001/",
-  internalHost: "http://localhost:3001/internal/",
-  gas: 200000,
-  ttl: 55
-}
+    host: "http://localhost:3001/",
+    internalHost: "http://localhost:3001/internal/"
+};
 
-describe('Example Contract', () => {
+const decodeAddress = (key) => {
+    const address = Crypto.decodeBase58Check(key.split('_')[1]).toString('hex')
+    return `0x${address}`
+};
 
-  let owner;
+describe('MultiSig Contract', () => {
 
-  before(async () => {
-    const ownerKeyPair = wallets[0];
-    owner = await Ae({
-      url: config.host,
-      internalUrl: config.internalHost,
-      keypair: ownerKeyPair,
-      nativeMode: true,
-      networkId: 'ae_devnet'
+    let clientOne, adminTwo, contract;
+    let adminOneKeypair = wallets[0];
+    let adminTwoKeypair = wallets[1];
+    let nonAdminKeypair = wallets[2];
+    let receiverNonAdminKeypair = wallets[3];
+
+    before(async () => {
+        clientOne = await Ae({
+            url: config.host,
+            internalUrl: config.internalHost,
+            keypair: adminOneKeypair,
+            nativeMode: true,
+            networkId: 'ae_devnet'
+        });
+
+        adminTwo = await Ae({
+            url: config.host,
+            internalUrl: config.internalHost,
+            keypair: adminTwoKeypair,
+            nativeMode: true,
+            networkId: 'ae_devnet'
+        });
     });
 
-  })
+    it('Deploy and Initialize MultiSig Contract', async () => {
+        let contractSource = utils.readFileRelative('./contracts/MultiSig.aes', "utf-8"); // Read the aes file
 
-  it('Deploying MultiSig Contract', async () => {
-    let contractSource = utils.readFileRelative('./contracts/MultiSig.aes', "utf-8"); // Read the aes file
+        const compiledContract = await clientOne.contractCompile(contractSource);
 
-    const compiledContract = await owner.contractCompile(contractSource, { // Compile it
-      gas: config.gas
-    })
+        const deployPromise = compiledContract.deploy({ // Deploy it
+            initState: `(${decodeAddress(adminOneKeypair.publicKey)}, ${decodeAddress(adminTwoKeypair.publicKey)})`
+        });
 
-    const deployPromise = compiledContract.deploy({ // Deploy it
-      options: {
-        ttl: config.ttl,
-      }
+        await assert.isFulfilled(deployPromise, 'Could not deploy the MultiSig Smart Contract'); // Check it is deployed
+        contract = await deployPromise;
     });
 
-    await assert.isFulfilled(deployPromise, 'Could not deploy the MultiSig Smart Contract'); // Check it is deployed
-  })
+    it('MultiSig Contract, both admins agree to spend workflow', async () => {
+        await clientOne.spend(100000, contract.address.replace('ct_', 'ak_'));
+        const receiverBalance = await clientOne.balance(receiverNonAdminKeypair.publicKey);
+        const contractBalance = await clientOne.balance(contract.address);
+        console.log(receiverBalance, contractBalance);
+        await contract.call('add_data_and_spend_if_both_admins_agree', {args: `({spend_to_address = ${decodeAddress(receiverNonAdminKeypair.publicKey)}, spend_amount = 1000})`})
+            .catch(console.error)
+    });
 
-})
+});
