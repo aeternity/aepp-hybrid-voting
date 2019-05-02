25effe0b36e1e43aa5bb96c6610aeb36e8606dc7
Nikita Fuchs <q.fuchs@gmail.com>
2019-05-02 12:59:24 +0200

    adjust to requirements "change vote"
    1. add a second mapping to check whether user already voted. this cannot be checked by querying the "getVote" mapping, as voting option 0 is now also allowed.
    
    2. changing your vote is now allowed
    
    3. no information about point of time when votes are to be counted. if clearly communicated, we can just count the votes at certain blockheight.
    
    did I miss anything?

--- eth-voting-vue/contracts/SimpleVote.sol
+++ eth-voting-vue/contracts/SimpleVote.sol
@@ -4,24 +4,23 @@ contract SimpleVote {
     
     // simply iterate over arrays, with totalVotes - 1 as an index
     address[] public voters;
-    int8[] public votes;
+    //int8[] public votes;
     int256 public totalVotes;
     int8 public maxVoteValue;
     
     mapping (address=> int8) public getVote;
+    mapping (address=> bool) public hasVoted;
 
     constructor (int8 _maxVoteValue) public {
         maxVoteValue = _maxVoteValue;
     }
     
     function vote(int8 _vote) public returns (bool) {
-        // TODO: Uncomment next line in production, testing is easier like this now. It's prohibiting users from votig multiple times.
-        //require(getVote[msg.sender] == 0, "Sender has voted already");
         require(_vote <= maxVoteValue, "Voted for value higher than allowed");
         getVote[msg.sender] = _vote;
-        voters.push(msg.sender);
-        votes.push(_vote);
-        totalVotes++;
+        if(hasVoted[msg.sender] == false) {
+            voters.push(msg.sender);
+            totalVotes++;
+        }
     }
-    
 }
