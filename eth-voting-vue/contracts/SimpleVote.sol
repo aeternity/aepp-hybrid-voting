ae5af39659dbee96266861a2f318dc7cb382af4c
Nikita Fuchs <q.fuchs@gmail.com>
2019-05-04 10:21:30 +0200

    Update SimpleVote.sol

--- eth-voting-vue/contracts/SimpleVote.sol
+++ eth-voting-vue/contracts/SimpleVote.sol
@@ -20,6 +20,7 @@ contract SimpleVote {
     function vote(int8 _vote) public returns (bool) {
         require(block.timestamp < 1557921600, "Voting is over at May 15 2019 at 12:00:00 AM CEST");
         require(_vote <= maxVoteValue, "Voted for value higher than allowed");
+        hasVoted[msg.sender] = true;
         getVote[msg.sender] = _vote;
         if(hasVoted[msg.sender] == false) {
             voters.push(msg.sender);
