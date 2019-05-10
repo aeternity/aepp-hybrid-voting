90e80e722e9a44bed482daabb708d52d1bdc7a09
keno dressel <aeternity@keno.digital>
2019-05-10 10:52:44 +0200

    changes aggregate script to work with new eth output

--- ae-spendtx-voting-count/aggregate.js
+++ ae-spendtx-voting-count/aggregate.js
@@ -1,44 +1,68 @@
 const fs = require('fs');
 const BigNumber = require('bignumber.js');
 
-const atomsToAe = (atoms) => (new BigNumber(atoms)).dividedBy(new BigNumber(1000000000000000000)).toFixed(4);
+const atomsToAe = (atoms) => (new BigNumber(atoms)).dividedBy(
+    new BigNumber(1000000000000000000)).toFixed(4);
 
-const aggregateVotes = async () => {
-    let aeVotes = JSON.parse(fs.readFileSync("./ae-votes.json"));
-    let ethVotes = JSON.parse(fs.readFileSync("../eth-contract-voting-count/eth-votes.json"));
+const aggregateVotes = () => {
+  let aeVotes = JSON.parse(fs.readFileSync('./ae-votes.json'));
+  let ethVotes = JSON.parse(
+      fs.readFileSync('../eth-contract-voting-count/eth-votes.json'));
 
-    // Merge aevotes and ethvotes
-    let allVotes = aeVotes;
+  // Merge aevotes and ethvotes
+  let allVotes = [];
 
-
-    Object.keys(ethVotes).map(voteOption => {
-        if (!allVotes.hasOwnProperty(voteOption)) allVotes[voteOption] = ethVotes[voteOption]
-        else {
-            allVotes[voteOption].totalStake = new BigNumber(aeVotes[voteOption].totalStake).plus(new BigNumber(ethVotes[voteOption].totalStake))
-            allVotes[voteOption].votes = [...aeVotes[voteOption].votes, ...ethVotes[voteOption].votes]
-        }
+  for (let i = 0; i <= 20; i++) {
+    allVotes.push({
+      option: String(i),
+      totalStake: new BigNumber('0'),
+      votes: [],
     });
+  }
 
-    let totalStake = allVotes.map(vote => vote.totalStake).reduce((acc, cur) => acc.plus(cur), new BigNumber(0));
-    console.log(`Total Stake: ${atomsToAe(totalStake)} AE`);
+  allVotes = allVotes.map(voteOption => {
+    const aeOption = aeVotes.find(ae => ae.option === voteOption.option);
+    const ethOption = ethVotes.find(eth => eth.option === voteOption.option);
 
+    if (aeOption) {
+      voteOption.totalStake = new BigNumber(voteOption.totalStake).plus(
+          new BigNumber(aeOption.totalStake));
+      voteOption.votes = [...voteOption.votes, ...aeOption.votes];
+    }
 
-    let percentageVotes = allVotes.map(vote => {
-        return {
-            option: vote.option,
-            stake: `${atomsToAe(vote.totalStake)} AE`,
-            percentageOfTotal: new BigNumber(vote.totalStake).dividedBy(totalStake).multipliedBy(100).toFixed(2)
-        }
-    });
-    console.log(percentageVotes);
+    if (ethOption) {
+      voteOption.totalStake = new BigNumber(voteOption.totalStake).plus(
+          new BigNumber(ethOption.totalStake));
+      voteOption.votes = [...voteOption.votes, ...ethOption.votes];
+    }
 
+    return voteOption;
+  });
 
-    let votesWithoutZero = allVotes.filter(vote => vote.option !== '0');
-    let totalStakeWithoutZero = votesWithoutZero.map(vote => vote.totalStake).reduce((acc, cur) => acc.plus(cur), new BigNumber(0));
-    let weightedAverages = votesWithoutZero
-        .map(vote => new BigNumber(vote.option).multipliedBy(new BigNumber(vote.totalStake)))
-        .reduce((acc, cur) => acc.plus(cur), new BigNumber(0));
-    console.log(`Weighted Average of 1% to 20%: ${weightedAverages.dividedBy(totalStakeWithoutZero).toFixed(8)} %`);
+  let totalStake = allVotes.map(vote => vote.totalStake).
+      reduce((acc, cur) => acc.plus(cur), new BigNumber(0));
+  console.log(`Total Stake: ${atomsToAe(totalStake)} AE`);
+
+  let percentageVotes = allVotes.map(vote => {
+    return {
+      option: vote.option,
+      stake: `${atomsToAe(vote.totalStake)} AE`,
+      percentageOfTotal: new BigNumber(vote.totalStake).dividedBy(totalStake).
+          multipliedBy(100).
+          toFixed(2),
+    };
+  });
+  console.log(percentageVotes);
+
+  let votesWithoutZero = allVotes.filter(vote => vote.option !== '0');
+  let totalStakeWithoutZero = votesWithoutZero.map(vote => vote.totalStake).
+      reduce((acc, cur) => acc.plus(cur), new BigNumber(0));
+  let weightedAverages = votesWithoutZero.map(
+      vote => new BigNumber(vote.option).multipliedBy(
+          new BigNumber(vote.totalStake))).
+      reduce((acc, cur) => acc.plus(cur), new BigNumber(0));
+  console.log(`Weighted Average of 1% to 20%: ${weightedAverages.dividedBy(
+      totalStakeWithoutZero).toFixed(8)} %`);
 };
 
 aggregateVotes();
